6c98edb6156278671631f0d845b496d57fd8a397
Torsten Schmits <tschmits@tzi.de>
2017-05-27 17:11:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+first_xamarin_test
---
+++ first_xamarin_test/first_xamarin_testPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace first_xamarin_test
+{
+    public partial class first_xamarin_testPage : ContentPage
+    {
+        public first_xamarin_testPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
